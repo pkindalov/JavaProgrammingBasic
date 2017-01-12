7a6f12a4d1f749fb94d70a178109caf40035c5c1
pkindalov <panayotkindalov@gmail.com>
2017-01-13 01:09:57 +0200

    Added exercise Point in rectangle

---
+++ MoreComplexConditions/PointInRectangle.java
@@ -0,0 +1,32 @@
+package MoreComplexConditions;
+
+import javax.xml.crypto.dom.DOMCryptoContext;
+import java.util.Scanner;
+
+/**
+ * Created by r3v3nan7 on 07.01.17.
+ */
+public class PointInRectangle {
+
+    public static void main(String[] args){
+        Scanner console = new Scanner(System.in);
+
+        double x1 = Double.parseDouble(console.nextLine());
+        double y1 = Double.parseDouble(console.nextLine());
+
+        double x2 = Double.parseDouble(console.nextLine());
+        double y2 = Double.parseDouble(console.nextLine());
+
+        double x = Double.parseDouble(console.nextLine());
+        double y = Double.parseDouble(console.nextLine());
+
+
+        if((x >= x1 && x <= x2) && (y >= y1 && y <= y2)){
+            System.out.println("Inside");
+        }else {
+            System.out.println("Outside");
+        }
+
+    }
+
+}
