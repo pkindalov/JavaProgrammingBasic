ffc147e7ad1f70394952b77808193ab79f535ebc
pkindalov <panayotkindalov@gmail.com>
2017-01-15 17:58:31 +0200

    Added exercise LatinLetters

---
+++ Loops/LatinLetters.java
@@ -0,0 +1,17 @@
+package Loops;
+
+/**
+ * Created by r3v3nan7 on 08.01.17.
+ */
+public class LatinLetters {
+
+    public static void main(String[] args){
+
+        for (char character = 'a'; character <= 'z' ; character++) {
+            System.out.print(character + " ");
+            //System.out.println(character + "-> " + (int)character);
+        }
+
+    }
+
+}
