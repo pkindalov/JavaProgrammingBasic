de911bf52022b4faff133d522500113e3dcf5927
pkindalov <panayotkindalov@gmail.com>
2017-01-16 11:16:34 +0200

    Added exercise Number ending in 7 one more version

---
+++ Loops/NumbersEndingIn7AnotherWay.java
@@ -0,0 +1,16 @@
+package Loops;
+
+/**
+ * Created by r3v3nan7 on 08.01.17.
+ */
+public class NumbersEndingIn7AnotherWay {
+
+    public static void main(String[] args){
+
+        for (int i = 7; i <= 1000 ; i+= 10) {
+            System.out.println(i);
+        }
+
+    }
+
+}
