fe80813ffcb2791dd3a06e8f9d9af6ceab99ade2
pkindalov <panayotkindalov@gmail.com>
2017-01-17 03:00:09 +0200

    Added exercise Numbers from 1 to 100

---
+++ Loops/NumbersFrom1To100.java
@@ -0,0 +1,16 @@
+package Loops;
+
+/**
+ * Created by r3v3nan7 on 08.01.17.
+ */
+public class NumbersFrom1To100 {
+
+    public static void main(String[] args){
+
+        for (int i = 1; i <= 100 ; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+}
