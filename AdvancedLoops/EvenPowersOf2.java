913db3bfab00595d03f3c627ce939b5f2807a643
pkindalov <panayotkindalov@gmail.com>
2017-01-27 00:35:20 +0200

    Added exercise even powers of 2

---
+++ AdvancedLoops/EvenPowersOf2.java
@@ -0,0 +1,21 @@
+package AdvancedLoops;
+
+import java.util.Scanner;
+
+/**
+ * Created by r3v3nan7 on 10.01.17.
+ */
+public class EvenPowersOf2 {
+
+    public static void main(String[] args){
+        Scanner console = new Scanner(System.in);
+        int n = Integer.parseInt(console.nextLine());
+        int num = 1;
+
+        for (int i = 0; i <= n ; i+= 2) {
+            System.out.println(num);
+            num *= 4;
+        }
+    }
+
+}
