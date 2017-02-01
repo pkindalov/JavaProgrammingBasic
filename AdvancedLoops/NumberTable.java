72fa2b73659ea6bdf7e811f9b8a0f9b5607cb98e
pkindalov <panayotkindalov@gmail.com>
2017-02-01 10:33:26 +0200

    Added exercise Number table

---
+++ AdvancedLoops/NumberTable.java
@@ -0,0 +1,30 @@
+package AdvancedLoops;
+
+import java.util.Scanner;
+
+/**
+ * Created by r3v3nan7 on 10.01.17.
+ */
+public class NumberTable {
+
+    public static void main(String[] args){
+        Scanner console = new Scanner(System.in);
+        int n = Integer.parseInt(console.nextLine());
+
+
+        for (int row = 0; row < n ; row++) {
+            for (int col = 0; col < n ; col++) {
+                int num = row + col + 1;
+
+                if(num > n){
+                    num = (2 * n) - num;
+                }
+                System.out.print(num + " ");
+            }
+
+            System.out.println();
+        }
+
+    }
+
+}
