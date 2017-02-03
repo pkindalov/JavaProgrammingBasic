024ec4adb9f08acd6b248d2ba63dc75306e8a95d
pkindalov <panayotkindalov@gmail.com>
2017-02-03 12:44:18 +0200

    Added exercise Sequence 2k + 1

---
+++ AdvancedLoops/Sequence2kplus1.java
@@ -0,0 +1,21 @@
+package AdvancedLoops;
+
+import java.util.Scanner;
+
+/**
+ * Created by r3v3nan7 on 10.01.17.
+ */
+public class Sequence2kplus1 {
+
+    public static void main(String[] args){
+        Scanner console = new Scanner(System.in);
+        int n = Integer.parseInt(console.nextLine());
+        int k = 1;
+
+        while (k <= n){
+            System.out.println(k);
+            k = 2 * k + 1;
+        }
+    }
+
+}
